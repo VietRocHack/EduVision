8e8cd3f36a96532886230bb94bfdb73330a23dfc
Hoang Le <hoangledo2092004@gmail.com>
2024-03-30 14:39:47 -0400

    Hoang Le: update Join Meeting

--- src/pages/Teacher.js
+++ src/pages/Teacher.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Typography, Grid, Button } from "@mui/material";
+import { Typography, Grid, Button, Stack } from "@mui/material";
 import Paper from '@mui/material/Paper';
 import Card from '@mui/material/Card';
 import backgroundImage from "../assets/banner-bg.png";
@@ -7,7 +7,7 @@ import backgroundImage from "../assets/banner-bg.png";
 const Teacher = () => {
   return (
     <>
-    <Grid
+    {/* <Grid
       container
       spacing={4}
       sx={{backgroundColor: "black", backgroundImage: `url(${backgroundImage})`, justifyContent: "center", padding: "50px"}}
@@ -46,22 +46,57 @@ const Teacher = () => {
               <Typography>Question</Typography>
               <Button variant="contained" sx={{width: "15%"}}>Quizzing</Button>
             </Paper>
-            <Paper sx={{ marginBottom: 3, height: "15vh", display: "flex", justifyContent: "space-between", alignContent: "center", padding: "10px"}} elevation={3}>
-              <Typography>Question</Typography>
-              <Button variant="contained" sx={{width: "15%"}}>Quizzing</Button>
-            </Paper>
-            <Paper sx={{ marginBottom: 3, height: "15vh", display: "flex", justifyContent: "space-between", alignContent: "center", padding: "10px"}} elevation={3}>
-              <Typography>Question</Typography>
-              <Button variant="contained" sx={{width: "15%"}}>Quizzing</Button>
-            </Paper>
-            <Paper sx={{ marginBottom: 3, height: "15vh", display: "flex", justifyContent: "space-between", alignContent: "center", padding: "10px"}} elevation={3}>
-              <Typography>Question</Typography>
-              <Button variant="contained" sx={{width: "15%"}}>Quizzing</Button>
-            </Paper>
+            
           </Grid>
         </Card>
       </Grid>
-    </Grid>
+      
+      
+    </Grid> */}
+
+    <Stack
+      direction="row" // or "column" depending on your layout needs
+      spacing={4}
+      sx={{
+        backgroundColor: "black",
+        backgroundImage: `url(${backgroundImage})`,
+        justifyContent: "center",
+        padding: "50px"
+      }}
+    >
+      <Card sx={{height: "90vh", borderRadius: "15px", width: "40%"}}>
+        <Stack direction="column" spacing={2} sx={{padding: "20px"}}>
+          <Paper sx={{ marginBottom: 2, height: "20vh" }} elevation={3}>Class Attendance</Paper>
+          <Paper sx={{ marginBottom: 2, height: "30vh" }} elevation={3}>Class Concentration</Paper>
+          <Paper sx={{ marginBottom: 2, height: "30vh" }} elevation={3}>Hard Concepts / Confusion Rate</Paper>
+        </Stack>
+      </Card>
+
+      <Card sx={{height: "100vh", borderRadius: "15px", width: "60%"}}>
+        <Stack direction="column" spacing={2} sx={{padding: "20px"}}>
+          <Paper sx={{ marginBottom: 3, height: "15vh", display: "flex", justifyContent: "space-between", alignContent: "center", padding: "10px"}} elevation={3}>
+            <Typography>Question</Typography>
+            <Button variant="contained" sx={{width: "15%", color: "white"}}>Quizzing</Button>
+          </Paper>
+          <Paper sx={{ marginBottom: 3, height: "15vh", display: "flex", justifyContent: "space-between", alignContent: "center", padding: "10px"}} elevation={3}>
+            <Typography>Question</Typography>
+            <Button variant="contained" sx={{width: "15%"}}>Quizzing</Button>
+          </Paper>
+          <Paper sx={{ marginBottom: 3, height: "15vh", display: "flex", justifyContent: "space-between", alignContent: "center", padding: "10px"}} elevation={3}>
+            <Typography>Question</Typography>
+            <Button variant="contained" sx={{width: "15%"}}>Quizzing</Button>
+          </Paper>
+          <Paper sx={{ marginBottom: 3, height: "15vh", display: "flex", justifyContent: "space-between", alignContent: "center", padding: "10px"}} elevation={3}>
+            <Typography>Question</Typography>
+            <Button variant="contained" sx={{width: "15%"}}>Quizzing</Button>
+          </Paper>
+          <Paper sx={{ marginBottom: 3, height: "15vh", display: "flex", justifyContent: "space-between", alignContent: "center", padding: "10px"}} elevation={3}>
+            <Typography>Question</Typography>
+            <Button variant="contained" sx={{width: "15%"}}>Quizzing</Button>
+          </Paper>
+        </Stack>
+      </Card>
+    </Stack>
   
     </>
   )
