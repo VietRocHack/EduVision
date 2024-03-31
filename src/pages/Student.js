916f4113adea9536fa656dceb18bbbc619447e95
Hoang Le <hoangledo2092004@gmail.com>
2024-03-31 10:10:28 -0400

    Hoang Le: update Class Content

--- src/pages/Student.js
+++ src/pages/Student.js
@@ -18,13 +18,60 @@ const Student = () => {
         padding: "50px"
       }}
     >
-      <Card sx={{height: "100vh", borderRadius: "15px", width: "50%"}}>
+      <Card sx={{height: "100%", borderRadius: "15px", width: "50%"}}>
         <Stack direction="column" spacing={2} sx={{padding: "20px"}}>
-          <Paper sx={{ marginBottom: 2, height: "30vh", display: "flex", justifyContent: "space-between", padding: "10px" }} elevation={3}>
-            <Paper elevation={3}></Paper>
-            <Paper elevation={3} sx={{width: "20%"}}></Paper>
+          <Paper elevation={3} sx={{height: "100%", display: "flex", alignItems: "center", textAlign: "center"}}>
+            0:00 - 0:10 
           </Paper>
-          
+          <Paper sx={{ marginBottom: 2, display: "flex", justifyContent: "space-between", padding: "10px", fontSize: "20px" }} elevation={3}>
+            Introduction to the concept of gazing and computer vision.
+            <br/>
+            Explanation of how these technologies intersect and complement each other.
+          </Paper>
+        </Stack>
+
+        <Stack direction="column" spacing={2} sx={{padding: "20px"}}>
+          <Paper elevation={3} sx={{height: "100%", display: "flex", alignItems: "center", textAlign: "center"}}>
+            0:10 - 0:20
+          </Paper>
+          <Paper sx={{ marginBottom: 2,  display: "flex", justifyContent: "space-between", padding: "10px", fontSize: "20px" }} elevation={3}>
+            Discussion on the role of computer vision in gazing applications.
+            <br/>
+            Examples of how computer vision enhances gazing systems for improved accuracy and functionality.
+          </Paper>
+        </Stack>
+
+        <Stack direction="column" spacing={2} sx={{padding: "20px"}}>
+          <Paper elevation={3} sx={{height: "100%", display: "flex", alignItems: "center", textAlign: "center"}}>
+            0:20 - 0:30 
+          </Paper>
+          <Paper sx={{ marginBottom: 2, height: "15vh", display: "flex", justifyContent: "space-between", padding: "10px", fontSize: "20px" }} elevation={3}>
+            Explanation of the significance of gazing in computer vision.
+            <br/>
+            How human gaze tracking can enhance computer vision algorithms for better understanding of user behavior.
+          </Paper>
+        </Stack>
+
+        <Stack direction="column" spacing={2} sx={{padding: "20px"}}>
+          <Paper elevation={3} sx={{height: "100%", display: "flex", alignItems: "center", textAlign: "center"}}>
+            0:10 - 0:20 
+          </Paper>
+          <Paper sx={{ marginBottom: 2, height: "15vh", display: "flex", justifyContent: "space-between", padding: "10px", fontSize: "20px" }} elevation={3}>
+            Insights into recent advancements in gazing and computer vision technologies.
+            <br/>
+            Discussion on challenges faced in integrating these technologies and ongoing research efforts to overcome them.
+          </Paper>
+        </Stack>
+
+        <Stack direction="column" spacing={2} sx={{padding: "20px"}}>
+          <Paper elevation={3} sx={{height: "100%", display: "flex", alignItems: "center", textAlign: "center"}}>
+            0:10 - 0:20 
+          </Paper>
+          <Paper sx={{ marginBottom: 2, height: "15vh", display: "flex", justifyContent: "space-between", padding: "10px", fontSize: "20px" }} elevation={3}>
+            Overview of the ethical considerations surrounding the use of gazing and computer vision technologies.
+            <br/>
+            Discussion on privacy concerns, data security, and potential biases in algorithmic decision-making.
+          </Paper>
         </Stack>
       </Card>
 
